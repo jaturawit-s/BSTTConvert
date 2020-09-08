7f8d7fa558beef7e07252b35bd4df9d9132d5adb
Jaturawit Sooksompong <jaturawit@verismart.co.th>
2020-09-08 14:24:00 +0700

    Replace sample data

--- BSTTConvert/src/BSTTConvert.java
+++ BSTTConvert/src/BSTTConvert.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
@@ -9,7 +10,18 @@ public class BSTTConvert {
     	System.out.print("Starting ...");
     	
     	try {
-			Scanner in = new Scanner(new FileReader("filename.txt"));
+			Scanner in0 = new Scanner(new FileReader("BSTTFiles/DM_BI_DLYSales_GT_New.csv"));
+			Scanner in1 = new Scanner(new FileReader("BSTTFiles/DM_BI_Stock_GT.csv"));
+			Scanner in2 = new Scanner(new FileReader("BSTTFiles/DM_M01_CUST.csv"));
+			Scanner in3 = new Scanner(new FileReader("BSTTFiles/DM_PENINV_GT.csv"));
+			Scanner in4 = new Scanner(new FileReader("BSTTFiles/"));
+			
+			System.out.println();
+			
+			
+			
+			
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
