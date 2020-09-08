9558ed637d0aab7538c5c1057b4d8425d530d3d9
Jaturawit Sooksompong <jaturawit@verismart.co.th>
2020-09-08 17:26:07 +0700

    utf8 to ascii working

--- BSTTConvert/src/BSTTConvert.java
+++ BSTTConvert/src/BSTTConvert.java
@@ -4,38 +4,34 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.util.Scanner;
+
 
 public class BSTTConvert {
-	// Connect to your database.
+	
     public static void main(String[] args)  {
     
     	System.out.println("Starting ...");
     	
     	try {
-//			Scanner in0 = new Scanner(new FileReader("BSTTFiles/DM_BI_DLYSales_GT.csv"));
-//			Scanner in1 = new Scanner(new FileReader("BSTTFiles/DM_BI_Stock_GT.csv"));
-//			Scanner in2 = new Scanner(new FileReader("BSTTFiles/DM_M01_CUST.csv"));
-//			Scanner in3 = new Scanner(new FileReader("BSTTFiles/DM_PENINV_GT.csv"));
+			
     		String file0 = "DM_BI_DLYSales_GT.csv";
     		String file1 = "DM_BI_Stock_GT.csv";
-    		String file2 = "DM_M01_CUST.csv";
-    		String file3 = "DM_PENINV_GT.csv";
+    		String file2 = "DM_PENINV_GT.csv";
+    		String file3 = "DM_M01_CUST.csv";
     		
-//			
+
 			
 			
-			InputStreamReader fr0 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file0), "UTF8");
-			InputStreamReader fr1 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file1), "UTF8");
-			InputStreamReader fr2 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file2), "UTF8");
-			InputStreamReader fr3 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file3), "UTF8");
+    		Reader fr0 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file0), "UTF8");
+			Reader fr1 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file1), "UTF8");
+			Reader fr2 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file2), "UTF8");
+			Reader fr3 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file3), "UTF8");
 				   
 			BufferedReader in0 = new BufferedReader(fr0);
 			BufferedReader in1 = new BufferedReader(fr1);
@@ -43,50 +39,51 @@ public class BSTTConvert {
 			BufferedReader in3 = new BufferedReader(fr3);
 			
 					
-			OutputStreamWriter fw0 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file0), StandardCharsets.UTF_8);
-			OutputStreamWriter fw1 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file1), StandardCharsets.UTF_8);
-			OutputStreamWriter fw2 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file2), StandardCharsets.UTF_8);
-			OutputStreamWriter fw3 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file3), StandardCharsets.UTF_8);
+			Writer fw0 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file0), "UTF8");
+			Writer fw1 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file1), "UTF8");
+			Writer fw2 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file2), "UTF8");
+			Writer fw3 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file3), "TIS620");
 			
 			BufferedWriter out0 = new BufferedWriter(fw0);
 			BufferedWriter out1 = new BufferedWriter(fw1);
 			BufferedWriter out2 = new BufferedWriter(fw2);
 			BufferedWriter out3 = new BufferedWriter(fw3);
 			
-			String str = "";
+			char cbuf[] = new char[2048];
+			int len;
 			
-			while ((str = in0.readLine()) != null) {
-	             System.out.println(str);
-	        }
+			while ((len = in0.read(cbuf, 0, cbuf.length)) != -1) {
+			    out0.write(cbuf, 0, len);
+			}
 			
 			in0.close();
+			out0.close();
 			
-			str = "";
-			
-			while ((str = in1.readLine()) != null) {
-	             System.out.println(str);
-	             out0.write(str);
-	        }
+			while ((len = in1.read(cbuf, 0, cbuf.length)) != -1) {
+			    out1.write(cbuf, 0, len);
+			}
 			
 			in1.close();
+			out1.close();
 			
-			str = "";
-			
-			while ((str = in2.readLine()) != null) {
-	             System.out.println(str);
-	             out0.write(str);
-	        }
+			while ((len = in2.read(cbuf, 0, cbuf.length)) != -1) {
+			    out2.write(cbuf, 0, len);
+			}
 			
 			in2.close();
+			out2.close();
 			
-			str = "";
-			
-			while ((str = in3.readLine()) != null) {
-	             System.out.println(str);
-	             out0.write(str);
-	        }
+			while ((len = in3.read(cbuf, 0, cbuf.length)) != -1) {
+			    out3.write(cbuf, 0, len);
+			    
+			    
+			    
+			}
 			
 			in3.close();
+			out3.close();
+			
+			
 			
 			
 			
@@ -108,5 +105,5 @@ public class BSTTConvert {
     	System.out.println("Done ...");
     	
     }
-
+    
 }
