e0c8f4e80288e17b315d8ccf062aa2c44b1696c2
Jaturawit Sooksompong <jaturawit@verismart.co.th>
2020-09-09 16:09:43 +0700

    use File.Seperator for multi os support

--- BSTTConvert/src/BSTTConvert.java
+++ BSTTConvert/src/BSTTConvert.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -8,7 +9,9 @@ import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.io.Writer;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BSTTConvert {
 
@@ -20,25 +23,30 @@ public class BSTTConvert {
 
 		try {
 
-			String file0 = "DM_BI_DLYSales_GT.csv";
-			String file1 = "DM_BI_Stock_GT.csv";
-			String file2 = "DM_PENINV_GT.csv";
-			String file3 = "DM_M01_CUST.csv";
+			String file0 = "DM_BI_DLYSales_GT";
+			String file1 = "DM_BI_Stock_GT";
+			String file2 = "DM_PENINV_GT";
+			String file3 = "DM_M01_CUST";
+			String ext = ".csv";
 
-			Reader fr0 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file0), "UTF8");
-			Reader fr1 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file1), "UTF8");
-			Reader fr2 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file2), "UTF8");
-			Reader fr3 = new InputStreamReader(new FileInputStream("BSTTFiles/" + file3), "UTF8");
+			Reader fr0 = new InputStreamReader(new FileInputStream("BSTTFiles" + File.separator + file0 + ext), "UTF8");
+			Reader fr1 = new InputStreamReader(new FileInputStream("BSTTFiles" + File.separator + file1 + ext), "UTF8");
+			Reader fr2 = new InputStreamReader(new FileInputStream("BSTTFiles" + File.separator + file2 + ext), "UTF8");
+			Reader fr3 = new InputStreamReader(new FileInputStream("BSTTFiles" + File.separator + file3 + ext), "UTF8");
 
 			BufferedReader in0 = new BufferedReader(fr0);
 			BufferedReader in1 = new BufferedReader(fr1);
 			BufferedReader in2 = new BufferedReader(fr2);
 			BufferedReader in3 = new BufferedReader(fr3);
 
-			Writer fw0 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file0), "UTF8");
-			Writer fw1 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file1), "UTF8");
-			Writer fw2 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file2), "UTF8");
-			Writer fw3 = new OutputStreamWriter(new FileOutputStream("BSTTFiles/Convert/" + file3), "TIS620");
+			Writer fw0 = new OutputStreamWriter(
+					new FileOutputStream("BSTTFiles/Convert/" + file0 + "_" + getDateFormat() + ext), "UTF8");
+			Writer fw1 = new OutputStreamWriter(
+					new FileOutputStream("BSTTFiles/Convert/" + file1 + "_" + getDateFormat() + ext), "UTF8");
+			Writer fw2 = new OutputStreamWriter(
+					new FileOutputStream("BSTTFiles/Convert/" + file2 + "_" + getDateFormat() + ext), "UTF8");
+			Writer fw3 = new OutputStreamWriter(
+					new FileOutputStream("BSTTFiles/Convert/" + file3 + "_" + getDateFormat() + ext), "TIS620");
 
 			BufferedWriter out0 = new BufferedWriter(fw0);
 			BufferedWriter out1 = new BufferedWriter(fw1);
@@ -52,15 +60,14 @@ public class BSTTConvert {
 					s = removeUTF8BOM(s);
 					firstLine = false;
 				}
-				
+
 				out0.write(s + "\n");
 				out0.flush();
-				
+
 			}
-			
+
 			out0.close();
 			in0.close();
-			
 
 			firstLine = true;
 			for (String s = ""; (s = in1.readLine()) != null;) {
@@ -71,38 +78,35 @@ public class BSTTConvert {
 
 				out1.write(s + "\n");
 				out1.flush();
-	
+
 			}
-			
+
 			out1.close();
 			in1.close();
-			
-			
+
 			firstLine = true;
 			for (String s = ""; (s = in2.readLine()) != null;) {
 				if (firstLine) {
 					s = removeUTF8BOM(s);
 					firstLine = false;
 				}
-				
+
 				out2.write(s + "\n");
 				out2.flush();
 
 			}
-			
+
 			out2.close();
 			in2.close();
-			
-			
+
 			for (String s = ""; (s = in3.readLine()) != null;) {
-				
+
 				out3.write(s + "\n");
 				out3.flush();
 			}
-			
+
 			out3.close();
 			in3.close();
-			
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -124,6 +128,15 @@ public class BSTTConvert {
 			s = s.substring(1);
 		}
 		return s;
+	}
+
+	private static String getDateFormat() {
+
+		DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+		Date date = new Date();
+		// System.out.println(dateFormat.format(date));
+
+		return (dateFormat.format(date)).toString();
 	}
 
 }
